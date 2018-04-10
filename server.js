e18329f3dbedba9fe60674141a91ef3b7de7ac64
Nikita Sasunkevich <nikita@nikitas-mbp.lan>
2018-04-09 22:36:58 -0700

    updated server.js with MONGODB_URI

--- server.js
+++ server.js
@@ -13,8 +13,10 @@ const exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/diggscraper";
+
 mongoose.Promise = Promise;
-mongoose.connect("mongodb://localhost/diggscraper");
+mongoose.connect(MONGODB_URI);
 
 require("./routes/routes")(app);
 
